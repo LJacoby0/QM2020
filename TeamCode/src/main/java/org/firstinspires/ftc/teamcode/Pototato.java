0d235575c881281448dc781f09d1024507b0d918
adamBoesky <apboesky@gmail.com>
2019-09-26 11:40:09 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pototato.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pototato.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pototato.java
@@ -67,6 +67,7 @@ public class Pototato extends LinearOpMode {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
+
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
