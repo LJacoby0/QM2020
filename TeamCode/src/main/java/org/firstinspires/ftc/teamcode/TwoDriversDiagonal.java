f3f9c799f11d043a3dc7ab6f9dfec44701b975c1
Todd <12956729+Tpixelminer@users.noreply.github.com>
2019-12-18 18:37:44 -0500

    o p t i m i z e
    sorry for making a seperate branch, was getting some wierd gradle errors and didnt want to break the rest of the working code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwoDriversDiagonal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwoDriversDiagonal.java
@@ -69,22 +69,35 @@ public class TwoDriversDiagonal extends LinearOpMode {
         }
     }
 
-    public void theDrive(double x_stick, double y_stick, double x_right_stick, double multiplier) {
-        double fbPower = x_stick;
-        double rlPower = y_stick;
-        rb.frontDrive.setPower(fbPower * multiplier + x_right_stick);
-        rb.backDrive.setPower(-fbPower * multiplier + x_right_stick);
-        rb.leftDrive.setPower(rlPower * multiplier + x_right_stick);
-        rb.rightDrive.setPower(-rlPower * multiplier + x_right_stick);
+    /**
+     * Main Diagonal Driving Function
+     * Basic Controls: Left stick is 360 degree motion and right stick is just rotation
+     * @param x_stick x value of left stick value -/+
+     * @param y_stick y value of left stick
+     * @param x_right_stick x value or right stick
+     * @param multiplier pre-determined multiplier in constants file, can be modified by slow mode
+     */
+
+    // diaDrive(leftX, leftY, rightX, pow);
+    public void diaDrive(double x_stick, double y_stick, double x_right_stick, double multiplier) {
+        //Calculates a power level for all motors rather than using pre-determined levels, allows for a mix of motors to be running for more control.
+        rb.frontDrive.setPower(x_stick * multiplier + x_right_stick);
+        rb.backDrive.setPower(-x_stick * multiplier + x_right_stick);
+        rb.leftDrive.setPower(y_stick * multiplier + x_right_stick);
+        rb.rightDrive.setPower(-y_stick * multiplier + x_right_stick);
+        //TODO: mess w/ multipliers
+        //TODO: Verify that this code actually works
     }
 
+
     private void drive() {
+        //Start w/ no movement
         double leftPower = 0;
         double rightPower = 0;
         double frontPower = 0;
         double backPower = 0;
         double power;
-
+        //Get Stick Values
         double leftY = gamepad1.left_stick_y;
         double leftX = gamepad1.left_stick_x;
         double rightX = gamepad1.right_stick_x;
@@ -92,16 +105,13 @@ public class TwoDriversDiagonal extends LinearOpMode {
         boolean rightb = gamepad1.right_bumper;
         boolean leftb = gamepad1.left_bumper;
 
-//        if (gamepad1.right_trigger >= Constants.TRIGGER_THRESHOLD) {
-//            power = Constants.DRIVE_POWER_SLOW;
-//        } else {
-//            power = Constants.DRIVE_POWER;
-//        }
+
         power = 1;
 
         // closed .84
         // open .46
-        //this moves spinning to the driver right stick and makes it more sensitive
+        //
+        // this moves spinning to the driver right stick and makes it more sensitive
 //        double allpower = 0;
 //        if (rightX < -0.13 || rightX > 0.13) {
 //            allpower = Range.clip(rightX, Constants.MIN_DRIVE_SPEED, Constants.MAX_DRIVE_SPEED);
@@ -111,7 +121,7 @@ public class TwoDriversDiagonal extends LinearOpMode {
 //            rb.leftDrive.setPower(allpower);
 //        }
 
-//
+        //idk why we even have these buttons but ok...
         if (rightb) {
             rb.frontDrive.setPower(Constants.SPIN_SPEED);
             rb.backDrive.setPower(Constants.SPIN_SPEED);
@@ -125,70 +135,35 @@ public class TwoDriversDiagonal extends LinearOpMode {
         }
 
 
-        /**
-         * Driving code.
-         *
-         * @param x_stick       The x value of the left joystick
-         * @param y_stick       The y value of the left joystick
-         * @param x_right_stick The x value of the right joystick
-         * @param multiplier    Number to multiply the power by.
-         */
+
+//Old vars
 //            float leftY = -gamepad1.left_stick_y;
 //            float leftX = gamepad1.left_stick_x;
 //            float rightX = gamepad1.right_stick_x;
 
 
+        //Slow mode code, should be irrelevant if variable speed is added to main code. If the driver prefers it tho, it can be used
         double pow;
         if (gamepad1.right_trigger >= TRIGGER_THRESHOLD) {
-            pow = DRIVE_POWER_SLOW;
+            pow = DRIVE_POWER_SLOW; //Slow constant
         } else {
             pow = DRIVE_POWER;
         }
 
+
+        //MAIN:
+        //Deadzone:  TODO: Make adjustable deadzone
+
+
         if (leftX * leftX + leftY * leftY >= DRIVE_STICK_THRESHOLD_SQUARED || Math.abs(rightX) >= DRIVE_STICK_THRESHOLD) {
-            theDrive(leftX, leftY, rightX, power);
+
+            diaDrive(leftX, leftY, rightX, pow);
+
+
         } else {
             rb.driveStop();
         }
 
-
-//        double fbPower = x_stick;
-//        double rlPower = y_stick;
-//        fMotor.setPower(fbPower * multiplier+x_right_stick);
-//        bMotor.setPower(-fbPower * multiplier+x_right_stick);
-//        lMotor.setPower(rlPower * multiplier+x_right_stick);
-//        rMotor.setPower(-rlPower * multiplier+x_right_stick);
-//
-//        if (rightX < -0.13 || rightX > 0.13) {
-//            frontPower = Range.clip(rightX, Constants.MIN_DRIVE_SPEED, Constants.MAX_DRIVE_SPEED);
-//            rb.frontDrive.setPower(frontPower * power);
-//            rb.backDrive.setPower(frontPower * power);
-//            rb.rightDrive.setPower(frontPower * power);
-//            rb.leftDrive.setPower(frontPower * power);
-//        }
-//        else if (leftY < -0.13 || leftY > 0.13) {
-//            telemetry.addData("controller Y is: ", "", leftY);
-//            //tried to add slow mode
-//            frontPower = Range.clip(leftY, Constants.MIN_DRIVE_SPEED, Constants.MAX_DRIVE_SPEED);
-//            rb.leftDrive.setPower(-frontPower * power);
-//            rb.rightDrive.setPower(frontPower * power);
-//            rb.frontDrive.setPower(0);
-//            rb.backDrive.setPower(0);
-//        } else if (leftX < -0.13 || leftX > 0.13) {
-//            telemetry.addData("controller X is: ", "", leftX);
-//            //tried to add slow mode
-//            leftPower = Range.clip(leftX, Constants.MIN_DRIVE_SPEED, Constants.MAX_DRIVE_SPEED);
-//            rb.leftDrive.setPower(0);
-//            rb.rightDrive.setPower(0);
-//            rb.frontDrive.setPower(leftPower * power);
-//            rb.backDrive.setPower(-leftPower * power);
-//        } else {
-//            //rb.driveStop();
-//            rb.leftDrive.setPower(0);
-//            rb.rightDrive.setPower(0);
-//            rb.frontDrive.setPower(0);
-//            rb.backDrive. setPower(0);
-//        }
 
         telemetry.addData("NS Motors", "front (%.2f), back (%.2f)", frontPower, backPower);
         telemetry.addData("Side Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
