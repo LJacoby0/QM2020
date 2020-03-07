3d97029d41aa272468fcb1e29008a4c53e87f0d1
Todd <contactsuperior.labs@gmail.com>
2020-03-07 13:49:10 -0500

    hikjl

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/feeder/feederautos/ColorAutoTestOperation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/feeder/feederautos/ColorAutoTestOperation.java
@@ -125,67 +125,150 @@ public abstract class ColorAutoTestOperation extends LinearOpMode {
             rb.setPlatformUp(true);
             rb.blockUp(); //Set Other Mechanisms out of the way
 
-            rb.driveForwardByEncoder(1370, rb.BL, .69);
-            telemetry.addData("2. Block Found Hue", hsvValues[0]);
-            telemetry.update();
-            runtime.reset();
-            //Pause after moving to block
-            while (runtime.seconds() < 1.0) { //TODO: Adjust this timing
-                rb.driveStop();
-            }
-            runtime.reset();
-            //Drive until block is black
-            do {
-                Color.RGBToHSV((int) (sensorColor.red() * SCALE_FACTOR),
-                        (int) (sensorColor.green() * SCALE_FACTOR),
-                        (int) (sensorColor.blue() * SCALE_FACTOR),
-                        hsvValues);
-                rb.strafe(blueNegativeFactor * 0.25);
-            }
-            while (hsvValues[0] <= 103);
-            {
-            }
 
-            double backToBlockBeginning = runtime.seconds();
-            runtime.reset();
+            rb.driveForwardByEncoder(-1400, rb.BL, .90);
 
-            rb.driveForwardByEncoder(200, rb.BL, .20); //Go forward a little bit
+            rb.intakeIn(1);
 
-            rb.blockDown(); //Grab first skystone //TODO: Fix this
+            rb.turnClockwiseByEncoder(-1100, rb.BL,.50);
 
-            sleep(1500); //stop  after finding block for servo to go down
+            rb.driveForwardByEncoder(-400, rb.BL, .20);
 
-            rb.driveForwardByEncoder(-274, rb.BL, .5); //Back Up a little
+            rb.driveForwardByEncoder(800, rb.BL, .90);
 
-            runtime.reset(); //Reset Time
-            while (runtime.seconds()<backToBlockBeginning) {
-                rb.strafe(blueNegativeFactor * -0.25); //go Back to beginning of block line
-            }
+            rb.turnClockwiseByEncoder(1100, rb.BL,.50 );
 
-            int strafeToMidPoint = (int) (4346 * blueNegativeFactor);
-            rb.strafeRightByEncoder(strafeToMidPoint, rb.BL, .7); //go to midpoint for blocks //TODO: Check to see where this ends up
-            rb.blockUp(); //Eject skystone
+            rb.strafeRightByEncoder(-1600, rb.BL, 1);
+
+            rb.intakeOut();
+
             sleep(1000);
-            int strafeToPlatform = (int) (2173 * blueNegativeFactor);
-            rb.strafeRightByEncoder(strafeToPlatform, rb.BL, .7); //go to platform for blocks //TODO: Check to see where this ends up
 
-            rb.driveForwardByEncoder(300,rb.BL, .18);//Move slightly into platform //TODO: Check to see where this ends up
+            rb.strafeRightByEncoder(3000, rb.BL, 1);
 
-            rb.setPlatformUp(false); //Put platform grabbers down
+            rb.turnClockwiseByEncoder(-450, rb.BL,.50 );
 
-            rb.driveForwardByEncoder(-500,rb.BL, .20); //Move backwards a little TODO: check values
 
-            int turnTo = (int) (2173 * blueNegativeFactor);
-            rb.turnClockwiseByEncoder(turnTo, rb.BL, 0.14); //Rotate 90  TODO: find values for rotate
 
-            runtime.reset();
-            while (runtime.seconds() < 6) {
-                rb.tapeOut();
-                rb.driveForwardByEncoder(-500, rb.BL, 0.40); //Push into wall TODO: find values
 
-            }
 
-            rb.setPlatformUp(true); //Lift platform pullers before end of auto
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//            telemetry.addData("2. Block Found Hue", hsvValues[0]);
+//            telemetry.update();
+//            runtime.reset();
+//            //Pause after moving to block
+//            while (runtime.seconds() < 1.0) { //TODO: Adjust this timing
+//                rb.driveStop();
+//            }
+//            runtime.reset();
+//            //Drive until block is black
+////            do {
+////                Color.RGBToHSV((int) (sensorColor.red() * SCALE_FACTOR),
+////                        (int) (sensorColor.green() * SCALE_FACTOR),
+////                        (int) (sensorColor.blue() * SCALE_FACTOR),
+////                        hsvValues);
+////                rb.strafe(blueNegativeFactor * 0.25);
+////            }
+//
+//            boolean inBlock = true;
+//            int blockNumber = 0;
+//            int blockInterval = (int) (blueNegativeFactor *700);
+//
+//            while (inBlock== true)
+//            {
+//                rb.strafeRightByEncoder(blockInterval, rb.BL, 0.25);
+//                sleep(2000);
+//
+//                Color.RGBToHSV((int) (sensorColor.red() * SCALE_FACTOR),
+//                        (int) (sensorColor.green() * SCALE_FACTOR),
+//                        (int) (sensorColor.blue() * SCALE_FACTOR),
+//                        hsvValues);
+//
+//                if (hsvValues[0] <= 110) {
+//                    inBlock=true;
+//                    blockNumber++;
+//
+//                }
+//                else {
+//                    inBlock=false;
+//                }
+//            }
+//
+//
+//            sleep(9000);
+//
+//            double backToBlockBeginning = runtime.seconds();
+//            runtime.reset();
+//            int alignBlockGrabber = (int) (-275 * blueNegativeFactor);
+//            rb.strafeRightByEncoder(alignBlockGrabber,rb.BL ,20);
+//
+//            rb.driveForwardByEncoder(500, rb.BL, .20); //Go forward a little bit
+//
+//
+//            sleep(500);
+//            rb.blockDown(); //Grab first skystone //TODO: Fix this
+//
+//            sleep(1500); //stop  after finding block for servo to go down
+//
+//            rb.driveForwardByEncoder(-750, rb.BL, .5); //Back Up a little
+//
+//            runtime.reset(); //Reset Time
+//            int backToBlock = (int) (-700* blockNumber * blueNegativeFactor);
+//                rb.strafeRightByEncoder(backToBlock, rb.BL, .25); //go Back to beginning of block line
+//
+//
+//            int strafeToMidPoint = (int) (-3350 * blueNegativeFactor);
+//            rb.strafeRightByEncoder(strafeToMidPoint, rb.BL, .7); //go to midpoint for blocks //TODO: Check to see where this ends up
+//            rb.blockUp(); //Eject skystone
+//            sleep(1000);
+//
+//            int strafeToPlatform = (int) (-1860 * blueNegativeFactor);
+//            rb.strafeRightByEncoder(strafeToPlatform, rb.BL, .7); //go to platform for blocks //TODO: Check to see where this ends up
+//
+//            rb.driveForwardByEncoder(1200,rb.BL, .18);//Move slightly into platform //TODO: Check to see where this ends up
+//
+//            rb.setPlatformUp(false); //Put platform grabbers down
+//            sleep(1000);
+//            rb.driveForwardByEncoder(-1500,rb.BL, .59); //Move backwards a little TODO: check values
+//
+//            int turnTo = (int) (-1900 * blueNegativeFactor);
+//            rb.turnClockwiseByEncoder(turnTo, rb.BL, 0.20); //Rotate 90  TODO: find values for rotate
+//
+//            runtime.reset();
+//            while (runtime.seconds() < 4) {
+//                rb.tapeOut();
+//                //rb.driveForwardByEncoder(-1000, rb.BL, 0.60); //Push into wall TODO: find values
+//
+//            }
+//
+//            rb.setPlatformUp(true); //Lift platform pullers before end of auto
 
             stop();
 
